fcf9f6eae60f0a40707b36f5c4502b94d11c6023
Caroline Forslund <cf222ev@student.lnu.se>
2020-05-27 16:17:01 +0100

    app updated  to class component

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-function App() {
+class App extends React.Component {
 
-  //Trying to connect to the api
-  axios.get('http://localhost:5000/api/users')
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
+  state = {};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          Display data from API here.
-      </header>
-    </div>
-  );
+  componentDidMount() {
+     //Trying to connect to the api
+     axios.get('http://localhost:5000/api/users')
+     .then(response => {
+       console.log(response);
+     })
+     .catch(error => {
+       console.log('Error fetching and parsing data', error);
+     })
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+            Display data from API here.
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
